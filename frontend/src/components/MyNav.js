b18a16f02c82fc19e0643a144767c1636d195efc
Sergiu Buruiana <sergiu.buru99@gmail.com>
2022-09-29 14:40:48 -0500

    added a navbar component

---
+++ frontend/src/components/MyNav.js
@@ -0,0 +1,30 @@
+// import { Link } from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuthContext'
+
+// Boostrap components
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container, Nav, Navbar, Button} from 'react-bootstrap';
+
+const MyNav = () => {
+  const { user } = useAuthContext()
+  
+  return (
+    <Navbar fixed="top" bg="light">
+      <Container>
+        <Navbar.Brand >Travel Media</Navbar.Brand>
+        <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/vacations">Vacations</Nav.Link>
+          </Nav>
+          {user && (
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>{user.email}</Navbar.Text>
+              <Button className="logout-btn" variant="outline-dark">Logout</Button>
+          </Navbar.Collapse>
+          )}
+      </Container>
+    </Navbar>
+  )
+}
+
+export default MyNav
