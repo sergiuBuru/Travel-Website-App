e83009edaea47a6c76748978069835fb3322f9a4
Sergiu Buruiana <sergiu.buru99@gmail.com>
2022-09-29 14:57:24 -0500

    Added routes for the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import MyNav from './components/MyNav'
+import Home from './pages/Home'
+import Vacations from './pages/Vacations'
 
 function App() {
   return (
     <div className="App">
-      <MyNav />
+      <BrowserRouter>
+        <MyNav />
+        <div className='pages'>
+          <Routes>
+            <Route
+              path="/"
+              element= <Home />
+            />
+          </Routes>
+          <Routes>
+            <Route
+              path="/vacations"
+              element= <Vacations />
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
