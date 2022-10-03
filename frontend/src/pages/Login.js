b6afdd1f3e1896eb7ac6d2cff42d82f3ed5ff2e7
Sergiu Buruiana <sergiu.buru99@gmail.com>
2022-10-02 20:44:49 -0500

    Added login and signup pages

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,8 @@
+
+const Login = () => {
+  return (
+    <div className="login-container">Login page</div>
+  )
+}
+
+export default Login
---
+++ frontend/src/pages/Signup.js
@@ -0,0 +1,8 @@
+
+const Signup = () => {
+  return (
+    <div className="signup-container">Signup page</div>
+  )
+}
+
+export default Signup
