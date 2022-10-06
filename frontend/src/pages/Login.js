13a9fd2dfbbff5528ddac8c7ed581950138dce86
Sergiu Buruiana <sergiu.buru99@gmail.com>
2022-10-05 20:22:31 -0500

    Adding login functionality

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,39 @@
+import { useState } from "react"
+import { useLogin } from "../hooks/useLogin"
+
+import { Stack, TextField, Button, Alert} from "@mui/material"
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const {login, error, isLoading} = useLogin()
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    await login(email, password)
+  }
+
   return (
-    <div className="login-container">Login page</div>
+    <div className="login-container">
+      {error && <Alert severity="error">{error}</Alert>}
+      <div className="login-div">
+        <Stack spacing={3} className="login-stack">
+          <TextField 
+            id="outlined-basic" 
+            label="email" 
+            variant="outlined"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField 
+            id="outlined-basic"
+            label="password"
+            variant="outlined"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button variant="contained" disabled={isLoading} onClick={handleLogin}>Log in</Button>
+        </Stack>
+      </div>
+    </div>
   )
 }
 
