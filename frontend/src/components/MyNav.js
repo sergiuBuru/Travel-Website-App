93eb8b3496e81be33531e3b590dc7ff41e3078fc
Sergiu Buruiana <sergiu.buru99@gmail.com>
2022-10-05 20:15:22 -0500

    When the user logs out nagivate to home page

--- frontend/src/components/MyNav.js
+++ frontend/src/components/MyNav.js
@@ -1,13 +1,22 @@
-// import { Link } from 'react-router-dom'
 import { useAuthContext } from '../hooks/useAuthContext'
+import { useLogout } from '../hooks/useLogout';
+import { useNavigate } from 'react-router-dom';
 
 // Boostrap components
 import 'bootstrap/dist/css/bootstrap.css';
 import { Container, Nav, Navbar, Button} from 'react-bootstrap';
 
+
 const MyNav = () => {
+  const navigate = useNavigate()
   const { user } = useAuthContext()
+  const { logout } = useLogout()
   
+  const handleLogout = () => {
+    logout()
+    navigate('/')
+  }
+
   return (
     <Navbar  bg="light">
       <Container>
@@ -19,7 +28,11 @@ const MyNav = () => {
           {user && (
             <Navbar.Collapse className="justify-content-end">
               <Navbar.Text>{user.email}</Navbar.Text>
-              <Button className="logout-btn" variant="outline-dark" size="sm">Logout</Button>
+              <Button 
+                className="logout-btn"
+                variant="outline-dark"
+                size="sm"
+                onClick={handleLogout}>Logout</Button>
           </Navbar.Collapse>
           )}
       </Container>
