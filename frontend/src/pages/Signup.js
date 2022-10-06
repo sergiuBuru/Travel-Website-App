ba82086d3e04e916ab777355525aea35acdd6735
Sergiu Buruiana <sergiu.buru99@gmail.com>
2022-10-05 20:21:47 -0500

    Adding sign up functionality

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,7 +1,39 @@
+import { useState } from "react"
+import { useSignup } from "../hooks/useSignup"
+
+import { Stack, TextField, Button, Alert} from "@mui/material"
 
 const Signup = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const {signup, error, isLoading} = useSignup()
+
+  const handleSignup = async (e) => {
+    e.preventDefault()
+    await signup(email, password)
+  }
+
   return (
-    <div className="signup-container">Signup page</div>
+    <div className="signup-container">
+      {error && <Alert severity="error">{error}</Alert>}
+      <div className="signup-div">
+        <Stack spacing={3} className="signup-stack">
+          <TextField 
+            id="outlined-basic" 
+            label="email" 
+            variant="outlined"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField 
+            id="outlined-basic"
+            label="password"
+            variant="outlined"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button variant="contained" disabled={isLoading} onClick={handleSignup}>Sign up</Button>
+        </Stack>
+      </div>
+    </div>
   )
 }
 
