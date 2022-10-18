686f8b6b2cf732508fc592edbe58b498679b8b03
Sergiu Buruiana <sergiu.buru99@gmail.com>
2022-10-17 21:23:42 -0500

    Added vacation page where vacations are displayed in card components

--- frontend/src/pages/Vacations.js
+++ frontend/src/pages/Vacations.js
@@ -1,7 +1,54 @@
+import { useNavigate } from "react-router-dom"
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useEffect } from "react";
+import { useState } from "react";
+
+// components
+import { Fab, Button } from "@mui/material"
+import AddIcon from '@mui/icons-material/Add';
+import VacationCard from "../components/VacationCard";
 
 const Vacations = () => {
+  const navigate = useNavigate()
+  const { user } = useAuthContext()
+  const [vacations, setVacations] = useState([])
+
+  useEffect( () => {
+    // Fetch the vacations
+    const response = fetch('/vacations', {
+      headers: {'Authorization' : `Bearer ${user.token}`}
+    })
+    .then(response => {
+      if(!response.ok) {
+        throw new Error('HTTP error')
+      }
+      return response.json()
+    })
+    .then(vacations => setVacations(vacations))
+    .catch(err => console.log(err))
+  }, [user.token])
+
+  const handleClick = () => {
+    navigate('/add-vacation')
+  }
+
   return (
-    <div>Vacations</div>
+    <div className="vacations-container">
+      <div className="vacations-container">
+        {vacations && vacations.map((vacation) => (
+          <VacationCard key={vacation._id} vacation={vacation} />
+        ))}
+      </div> 
+      <div className="add-button-container">
+        <Fab 
+            color="primary" 
+            aria-label="add" 
+            className="add-vacations-button"
+            onClick={handleClick}>
+          <AddIcon />
+        </Fab>
+      </div>
+    </div>
   )
 }
 
