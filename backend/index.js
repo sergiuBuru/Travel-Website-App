fc5dd1dee717512d3a4fe83d451be20006790321
Sergiu Buruiana <sergiu.buru99@gmail.com>
2022-11-14 20:45:44 -0600

    Added .env file. Added home route and favicon route

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,15 @@ const mongoose = require('mongoose')
 const homeRoutes = require('./routes/home')
 const userRoutes = require('./routes/user')
 const vacationRoutes = require('./routes/vacations')
-
+const servefavicon = require('serve-favicon')
 
 // create the express app
 const app = express()
 
 // middleware
 app.use(express.json())
+
+// app.use(favicon(__dirname + '/favicon.ico'));
 
 // print the path and method of incoming requests
 app.use((req, res, next) => {
@@ -20,6 +22,9 @@ app.use((req, res, next) => {
 })
 
 // routes
+app.get('/', (req, res) => {
+  res.send('hello traveler')
+})
 app.use('/home', homeRoutes)
 app.use('/user', userRoutes)
 app.use('/vacations', vacationRoutes)
